17395ab46904fae8a1aef73669550a6212e823aa
lauracp742 <lauracp742@gmail.com>
2024-07-09 20:46:25 -0500

    primer push

---
+++ login.js
@@ -0,0 +1,13 @@
+    /*function reconoce() {
+        console.log('funciona');
+        var element = document.getElementById("imagendeseguridad");
+        element.classList.add("rosa");
+      }*/
+      function reconoce() {
+        console.log('funciona')
+      var miElto = document.getElementsByClassName("imagenseguridad")[0];
+      var textbox = document.getElementsByClassName("textbox")[0];
+      miElto.className = "rosa";
+      textbox.className = "rosatext";
+      }
+
